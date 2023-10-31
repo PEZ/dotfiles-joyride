36fcea34e2db4b8e37dd3c82eb88b9b17534d9ef
Peter Strömberg <pez@pezius.com>
2023-10-31 12:10:40 +0100

    Make scrap.js loadable

--- test-files/scrap.js
+++ test-files/scrap.js
@@ -26,14 +26,26 @@ const { readFile } = require("fs").promises;
 // Top level await not supported
 // const packageJson = await readFile('/Users/pez/.config/joyride/sidecar/package.json', 'utf-8');
 // const config = JSON.parse(packageJson);
-const config = (async () => {
-  const config = JSON.parse(
+
+// This makes `packageJson` print as a string in the repl, but it's a promise
+const packageJson = (async () => {
+  return await readFile("/Users/pez/.config/joyride/sidecar/package.json", "utf-8");
+});
+// this gives SyntaxError: Unexpected token 'o', "[object Promise]" is not valid JSON
+// const config = JSON.parse(packageJson); 
+// (You can select it, and evaluate and prove it for yourself)
+
+(async () => {
+  var config = JSON.parse(
     await readFile("/Users/pez/.config/joyride/sidecar/package.json", "utf-8")
   );
+  globalThis.config = config;
   return config;
 })();
-
-config.then((config) => config);
+config; // prints as an object in the repl, but it's a promise
+config.name; // null
+//c_name = config.then(x => x.name); // we need to do this to see the name
+typeof c_name; // object (it's still a promise)
 
 var {
   hello_fine,
@@ -41,4 +53,4 @@ var {
 } = require("/Users/pez/.config/joyride/test-files/has-errors.js");
 
 hello_fine("World");
-hello_borked("World");
+//hello_borked("World");
